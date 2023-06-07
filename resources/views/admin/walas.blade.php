b85d9412f8d325c15ef7632013e0a66035b394b4
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2023-06-07 07:14:53 +0700

    Fixing Walas Page

--- resources/views/admin/walas.blade.php
+++ resources/views/admin/walas.blade.php
@@ -1,44 +1,89 @@
 @extends('layout.template.dashboard')
+
+@section('title-tab', 'LifeGuidance')
+
+@section('title-table', 'Data Guru Bimbingan Konseling')
+
 @section('admin_content')
-<a href="{{ route('create_walas') }}">
-    <button type="button" class="btn btn-info">Create</button>
-</a>
-<table class="table">
-    <thead>
-      <tr>
-        <th scope="col">No</th>
-        <th scope="col">Nama</th>
-        <th scope="col">Email</th>
-        <th scope="col">NIP</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-        @php
-            $i = 1
-        @endphp
-        @foreach ($datas as $data)
-        <tr>
-            <td>{{ $i++ }}</td>
-            <td>{{ $data->name }}</td>
-            <td>{{ $data->email }}</td>
-            <td>{{ $data->nip }}</td>
-            <td >
-                <form action="walas/destroy/{{ $data->id }}" method="POST">
-                    @csrf
-                    <button class="btn btn-icon btn-2 btn-danger" type="submit">
-                        <span class="btn-inner--icon"><i class="ni ni-bulb-61"></i></span>
-                    </button>
-                </form>
-                <a href="{{ route('edit_walas', $data->id) }}">
-                    <button class="btn btn-icon btn-2 btn-warning" type="button">
-                        <span class="btn-inner--icon"><i class="ni ni-bulb-61"></i></span>
-                    </button>
-                </a>
-            </td>
-        </tr>
-      @endforeach
-     
-    </tbody>
-  </table>
+    <div class="row">
+        <div class="col-12">
+            <div class="card mb-4">
+                <div class="card-header pb-0 d-flex justify-content-between align-items-center">
+                    <h6>Guru Bimbingan Konseling</h6>
+                    <a href="{{ route('create_guru_bk') }}">
+                        <button type="button" class="btn bg-gradient-info btn-sm">Create</button>
+                    </a>
+                </div>
+                <div class="card-body px-0 pt-0 pb-2">
+                    <div class="table-responsive p-0">
+                        <table class="table align-items-center mb-0">
+                            <thead>
+                                <tr>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No</th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama
+                                    </th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Email
+                                    </th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">NIP</th>
+                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($datas as $data)
+                                    <tr>
+                                        <td>
+                                            <div class="ms-3 text-secondary">
+                                                <p class="text-secondary">{{ $loop->iteration }}</p>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="text-secondary">
+                                                <p class="text-secondary">{{ $data->name }}</p>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="text-secondary">
+                                                <p class="text-secondary">{{ $data->email }}</p>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="text-secondary">
+                                                <p class="text-secondary">{{ $data->nip }}</p>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="text-secondary d-flex">
+                                                <form action="guru_bk/destroy/{{ $data->id }}" method="POST"
+                                                    >
+                                                    @csrf
+                                                    <button class="btn btn-icon btn-2 bg-gradient-danger me-3" type="submit">
+                                                        <span class="btn-inner--icon"><i class="ni ni-basket"></i></span>
+                                                    </button>
+                                                </form>
+                                                <a href="{{ route('edit_guru_bk', $data->id) }}">
+                                                    <button class="btn btn-icon btn-2 bg-gradient-warning" type="button">
+                                                        <span class="btn-inner--icon"><i class="ni ni-ruler-pencil"></i></span>
+                                                    </button>
+                                                </a>
+                                            </div>
+                                        </td>
+
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="5">
+                                            <div class="text-secondary">
+                                                <p class="text-secondary">Table Kosong</p>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
