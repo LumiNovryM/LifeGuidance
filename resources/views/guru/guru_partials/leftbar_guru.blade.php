1b3aa8c10af28c22f2d53c14a30db66f672866a0
LumiNovryM <luminovrymekel@gmail.com>
2023-06-08 09:59:17 +0700

    Fixing Slicing Dashboad For Walas, Guru, & Siswa

--- resources/views/guru/guru_partials/leftbar_guru.blade.php
+++ resources/views/guru/guru_partials/leftbar_guru.blade.php
@@ -10,7 +10,7 @@
     <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link {{ ($title == "Dashboard") ? 'active' : '' }}"  href="{{ route('home_guru') }}">
+          <a class="nav-link {{ ($title == "Dashboard") ? 'active' : '' }}"  href="">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>shop </title>
@@ -30,7 +30,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title == "Kelas") ? 'active' : '' }} " href="{{ route('show_kelas') }}">
+          <a class="nav-link {{ ($title == "Kelas") ? 'active' : '' }} " href="">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>office</title>
@@ -50,7 +50,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title == "Siswa") ? 'active' : '' }}" href="{{ route('show_siswa') }}">
+          <a class="nav-link {{ ($title == "Siswa") ? 'active' : '' }}" href="">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>box-3d-50</title>
@@ -71,7 +71,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ ($title == "Guru BK") ? 'active' : '' }}" href="{{ route('show_guru_bk') }}">
+          <a class="nav-link {{ ($title == "Guru BK") ? 'active' : '' }}" href="">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>credit-card</title>
