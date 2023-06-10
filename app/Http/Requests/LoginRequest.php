7c09a8db995a688708d08e1c397ad8b85bbe6027
dhykas <p0sp3s@gmail.com>
2023-06-10 08:02:27 +0700

    fix request validate api login

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class LoginRequest extends FormRequest
 {
@@ -17,5 +19,13 @@ class LoginRequest extends FormRequest
             'email' => 'required|email',
             'password' => 'required',
         ];
+    }
+
+    function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 400,
+            'Error' => $validator->errors()
+        ]));    
     }
 }
