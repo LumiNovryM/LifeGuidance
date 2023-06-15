60517237af2977e64d83811a173eae3fba02577f
LumiNovryM <luminovrymekel@gmail.com>
2023-06-15 18:34:08 +0700

    Add Japanese On Progress

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -1,12 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Stichoza\GoogleTranslate\GoogleTranslate;
 use Illuminate\Http\Request;
 
-class dashboardController extends Controller
+class DashboardController extends Controller
 {
-    public function dashboard(){
-        return view('layout.template.dashboard');
+    public function indonesia()
+    {
+        return view('layout.page.lang.id.landingpage');
+    }
+
+    public function japanese()
+    {
+        $lang = new ('id');
+        
     }
 }
