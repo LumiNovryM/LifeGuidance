40705199e5868cca1034c39d5442407e3b1eba36
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2023-07-29 17:32:27 +0700

    Update API

--- app/Http/Controllers/NoTokenApiController.php
+++ app/Http/Controllers/NoTokenApiController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Helpers\ApiFormatter;
-use App\Models\Bimbingan_Belajar;
-use App\Models\Bimbingan_Karir;
-use App\Models\Bimbingan_Pribadi;
-use App\Models\Bimbingan_Sosial;
 use Illuminate\Http\Request;
+use App\Helpers\ApiFormatter;
+use Illuminate\Support\Carbon;
+use App\Models\Bimbingan_Karir;
+use App\Models\Bimbingan_Sosial;
+use App\Models\Bimbingan_Belajar;
+use App\Models\Bimbingan_Pribadi;
 
 class NoTokenApiController extends Controller
 {
     //list
     public function bimbingan_pribadi($id)
     {
-        $datas = Bimbingan_Pribadi::with('siswa', 'guru', 'walas', 'kelas')->where('siswa_id', $id)->where('status', 'Selesai')->get();
+        $datas = Bimbingan_Pribadi::with('siswa', 'guru', 'walas', 'kelas')->where('siswa_id', $id)->get();
         if ($datas) {
             return ApiFormatter::createApi(200, 'Success', $datas);
         } else {
@@ -23,7 +24,7 @@ class NoTokenApiController extends Controller
     }
     public function bimbingan_belajar($id)
     {
-        $datas = Bimbingan_Belajar::with('siswa', 'guru', 'walas', 'kelas')->where('siswa_id', $id)->where('status', 'Selesai')->get();
+        $datas = Bimbingan_Belajar::with('siswa', 'guru', 'walas', 'kelas')->where('siswa_id', $id)->get();
         if ($datas) {
             return ApiFormatter::createApi(200, 'Success', $datas);
         } else {
@@ -32,7 +33,7 @@ class NoTokenApiController extends Controller
     }
     public function bimbingan_sosial($id)
     {
-        $datas = Bimbingan_Sosial::with('siswa', 'guru', 'walas', 'kelas')->where('siswa_id', $id)->where('status', 'Selesai')->get();
+        $datas = Bimbingan_Sosial::with('siswa', 'guru', 'walas', 'kelas')->where('siswa_id', $id)->get();
         if ($datas) {
             return ApiFormatter::createApi(200, 'Success', $datas);
         } else {
@@ -41,7 +42,7 @@ class NoTokenApiController extends Controller
     }
     public function bimbingan_karir($id)
     {
-        $datas = Bimbingan_Karir::with('siswa', 'guru', 'walas', 'kelas')->where('siswa_id', $id)->where('status', 'Selesai')->get();
+        $datas = Bimbingan_Karir::with('siswa', 'guru', 'walas', 'kelas')->where('siswa_id', $id)->get();
         if ($datas) {
             return ApiFormatter::createApi(200, 'Success', $datas);
         } else {
@@ -51,44 +52,287 @@ class NoTokenApiController extends Controller
 
 
 
+    // post data
+    public function tambah_bimbingan_pribadi(Request $request)
+    {
 
-    // detail api
-    public function detail_bimbingan_pribadi($id)
-    {
-        $datas = Bimbingan_Pribadi::with('siswa', 'guru', 'walas', 'kelas')->where('id', $id)->where('status', 'Selesai')->get();
-        if ($datas) {
-            return ApiFormatter::createApi(200, 'Success', $datas);
-        } else {
-            return ApiFormatter::createApi(400, 'Failed');
-        }
+        Bimbingan_Pribadi::insert([
+            'siswa_id' => $request->siswa_id,
+            'kelas_id' => $request->kelas_id,
+            'walas_id' => $request->walas_id,
+            'guru_id' => $request->guru_id,
+            'alasan_pertemuan' => $request->alasan_pertemuan,
+            'tanggal_pertemuan' => $request->tanggal_pertemuan,
+            'lokasi_pertemuan' => $request->lokasi_pertemuan,
+            'status' => 'Menunggu',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        return ApiFormatter::createApi(200, 'Data berhasil ditambahkan');
     }
-    public function detail_bimbingan_belajar($id)
+
+    public function tambah_bimbingan_belajar(Request $request)
     {
-        $datas = Bimbingan_Belajar::with('siswa', 'guru', 'walas', 'kelas')->where('id', $id)->where('status', 'Selesai')->get();
-        if ($datas) {
-            return ApiFormatter::createApi(200, 'Success', $datas);
-        } else {
-            return ApiFormatter::createApi(400, 'Failed');
-        }
+        Bimbingan_Belajar::insert([
+            'siswa_id' => $request->siswa_id,
+            'kelas_id' => $request->kelas_id,
+            'walas_id' => $request->walas_id,
+            'guru_id' => $request->guru_id,
+            'diajukan' => $request->diajukan,
+            'alasan_pertemuan' => $request->alasan_pertemuan,
+            'tanggal_pertemuan' => $request->tanggal_pertemuan,
+            'lokasi_pertemuan' => $request->lokasi_pertemuan,
+            'status' => 'Menunggu',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        return ApiFormatter::createApi(200, 'Data berhasil ditambahkan');
     }
-    public function detail_bimbingan_sosial($id)
+
+    public function tambah_bimbingan_sosial(Request $request)
     {
-        $datas = Bimbingan_Sosial::with('siswa', 'guru', 'walas', 'kelas')->where('id', $id)->where('status', 'Selesai')->get();
-        if ($datas) {
-            return ApiFormatter::createApi(200, 'Success', $datas);
-        } else {
-            return ApiFormatter::createApi(400, 'Failed');
-        }
+        Bimbingan_Sosial::insert([
+            'siswa_id' => $request->siswa_id,
+            'kelas_id' => $request->kelas_id,
+            'walas_id' => $request->walas_id,
+            'guru_id' => $request->guru_id,
+            'diajukan' => $request->diajukan,
+            'alasan_pertemuan' => $request->alasan_pertemuan,
+            'tanggal_pertemuan' => $request->tanggal_pertemuan,
+            'lokasi_pertemuan' => $request->lokasi_pertemuan,
+            'status' => 'Menunggu',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        return ApiFormatter::createApi(200, 'Data berhasil ditambahkan');
     }
-    public function detail_bimbingan_karir($id)
+
+    public function tambah_bimbingan_karir(Request $request)
     {
-        $datas = Bimbingan_Karir::with('siswa', 'guru', 'walas', 'kelas')->where('id', $id)->where('status', 'Selesai')->get();
-        if ($datas) {
-            return ApiFormatter::createApi(200, 'Success', $datas);
-        } else {
-            return ApiFormatter::createApi(400, 'Failed');
-        }
+        Bimbingan_Karir::insert([
+            'siswa_id' => $request->siswa_id,
+            'kelas_id' => $request->kelas_id,
+            'walas_id' => $request->walas_id,
+            'guru_id' => $request->guru_id,
+            'tipe_bimbingan' => $request->tipe_bimbingan,
+            'alasan_pertemuan' => $request->alasan_pertemuan,
+            'tanggal_pertemuan' => $request->tanggal_pertemuan,
+            'lokasi_pertemuan' => $request->lokasi_pertemuan,
+            'status' => 'Menunggu',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]); 
+
+        return ApiFormatter::createApi(200, 'Data berhasil ditambahkan');
     }
+
+
+
     
+    // delete data
+    public function hapus_bimbingan_pribadi($id)
+    {
+        $bimbingan = Bimbingan_Pribadi::find($id);
+
+        if (!$bimbingan) {
+            return ApiFormatter::createApi(404, 'Data tidak ditemukan');
+        }
+
+        $bimbingan->delete();
+
+        return ApiFormatter::createApi(200, 'Data berhasil dihapus');
+    }
+
+    public function hapus_bimbingan_belajar($id)
+    {
+        $bimbingan = Bimbingan_Belajar::find($id);
+
+        if (!$bimbingan) {
+            return ApiFormatter::createApi(404, 'Data tidak ditemukan');
+        }
+
+        $bimbingan->delete();
+
+        return ApiFormatter::createApi(200, 'Data berhasil dihapus');
+    }
+
+    public function hapus_bimbingan_sosial($id)
+    {
+        $bimbingan = Bimbingan_Sosial::find($id);
+
+        if (!$bimbingan) {
+            return ApiFormatter::createApi(404, 'Data tidak ditemukan');
+        }
+
+        $bimbingan->delete();
+
+        return ApiFormatter::createApi(200, 'Data berhasil dihapus');
+    }
+
+    public function hapus_bimbingan_karir($id)
+    {
+        $bimbingan = Bimbingan_Karir::find($id);
+
+        if (!$bimbingan) {
+            return ApiFormatter::createApi(404, 'Data tidak ditemukan');
+        }
+
+        $bimbingan->delete();
+
+        return ApiFormatter::createApi(200, 'Data berhasil dihapus');
+    }
+
+
+
+
+    // update data
+    public function ubah_bimbingan_pribadi(Request $request, $id)
+    {
+        $request->validate([
+            'alasan_pertemuan' => 'required',
+            'tanggal_pertemuan' => 'required',
+            'lokasi_pertemuan' => 'required',
+        ]);
+
+        // Find the data to update
+        $bimbingan_pribadi = Bimbingan_Pribadi::find($id);
+
+        if (!$bimbingan_pribadi) {
+            return ApiFormatter::createApi(404, 'Data not found');
+        }
+
+        // Update the data
+        $bimbingan_pribadi->update([
+            'alasan_pertemuan' => $request->alasan_pertemuan,
+            'tanggal_pertemuan' => $request->tanggal_pertemuan,
+            'lokasi_pertemuan' => $request->lokasi_pertemuan,
+            'updated_at' => Carbon::now()
+        ]);
+
+        return ApiFormatter::createApi(200, 'Data berhasil diubah');
+    }    
+    public function ubah_bimbingan_belajar(Request $request, $id)
+    {
+        $request->validate([
+            'alasan_pertemuan' => 'required',
+            'tanggal_pertemuan' => 'required',
+            'lokasi_pertemuan' => 'required',
+        ]);
+
+        // Find the data to update
+        $bimbingan_belajar = Bimbingan_Belajar::find($id);
+
+        if (!$bimbingan_belajar) {
+            return ApiFormatter::createApi(404, 'Data not found');
+        }
+
+        $bimbingan_belajar->update([
+            'alasan_pertemuan' => $request->alasan_pertemuan,
+            'tanggal_pertemuan' => $request->tanggal_pertemuan,
+            'lokasi_pertemuan' => $request->lokasi_pertemuan,
+            'updated_at' => Carbon::now()
+        ]);
+
+        return ApiFormatter::createApi(200, 'Data berhasil diubah');
+    }
+
+    public function ubah_bimbingan_sosial(Request $request, $id)
+    {
+        $request->validate([
+            'alasan_pertemuan' => 'required',
+            'tanggal_pertemuan' => 'required',
+            'lokasi_pertemuan' => 'required',
+        ]);
+
+        // Find the data to update
+        $bimbingan = Bimbingan_Sosial::find($id);
+
+        if (!$bimbingan) {
+            return ApiFormatter::createApi(404, 'Data not found');
+        }
+
+        $bimbingan->update([
+            'alasan_pertemuan' => $request->alasan_pertemuan,
+            'tanggal_pertemuan' => $request->tanggal_pertemuan,
+            'lokasi_pertemuan' => $request->lokasi_pertemuan,
+            'updated_at' => Carbon::now()
+        ]);
+
+        return ApiFormatter::createApi(200, 'Data berhasil diubah');
+    }
+
+    public function ubah_bimbingan_karir(Request $request, $id)
+    {
+        $request->validate([
+            'alasan_pertemuan' => 'required',
+            'tanggal_pertemuan' => 'required',
+            'lokasi_pertemuan' => 'required',
+        ]);
+
+        // Find the data to update
+        $bimbingan = Bimbingan_Karir::find($id);
+
+        if (!$bimbingan) {
+            return ApiFormatter::createApi(404, 'Data not found');
+        }
+
+        $bimbingan->update([
+            'alasan_pertemuan' => $request->alasan_pertemuan,
+            'tanggal_pertemuan' => $request->tanggal_pertemuan,
+            'lokasi_pertemuan' => $request->lokasi_pertemuan,
+            'updated_at' => Carbon::now()
+        ]);
+
+        return ApiFormatter::createApi(200, 'Data berhasil diubah');
+    }
+
+    // count
+    public function count_bimbingan_pribadi($id) {
+        $datas = Bimbingan_Pribadi::where('siswa_id', $id)->get();
+        $count = $datas->count();
+
+        if ($datas) {
+        return ApiFormatter::createApi(200, 'Success', $count);
+        } else {
+            return ApiFormatter::createApi(400, 'Failed');
+        }
+    }
+
+    public function count_bimbingan_belajar($id) {
+        $datas = Bimbingan_Belajar::where('siswa_id', $id)->get();
+        $count = $datas->count();
+
+        if ($datas) {
+        return ApiFormatter::createApi(200, 'Success', $count);
+        } else {
+            return ApiFormatter::createApi(400, 'Failed');
+        }
+    }
+
+    public function count_bimbingan_sosial($id) {
+        $datas = Bimbingan_Sosial::where('siswa_id', $id)->get();
+        $count = $datas->count();
+
+        if ($datas) {
+        return ApiFormatter::createApi(200, 'Success', $count);
+        } else {
+            return ApiFormatter::createApi(400, 'Failed');
+        }
+    }
+
+    public function count_bimbingan_karir($id) {
+        $datas = Bimbingan_Karir::where('siswa_id', $id)->get();
+        $count = $datas->count();
+
+        if ($datas) {
+        return ApiFormatter::createApi(200, 'Success', $count);
+        } else {
+            return ApiFormatter::createApi(400, 'Failed');
+        }
+    }
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -28,3 +28,26 @@ Route::get('/detail_bimbingan_pribadi/{id}', [NoTokenApiController::class, 'deta
 Route::get('/detail_bimbingan_belajar/{id}', [NoTokenApiController::class, 'detail_bimbingan_belajar']);
 Route::get('/detail_bimbingan_sosial/{id}', [NoTokenApiController::class, 'detail_bimbingan_sosial']);
 Route::get('/detail_bimbingan_karir/{id}', [NoTokenApiController::class, 'detail_bimbingan_karir']);
+
+Route::post('/tambah_bimbingan_pribadi', [NoTokenApiController::class, 'tambah_bimbingan_pribadi']);
+Route::post('/tambah_bimbingan_belajar', [NoTokenApiController::class, 'tambah_bimbingan_belajar']);
+Route::post('/tambah_bimbingan_sosial', [NoTokenApiController::class, 'tambah_bimbingan_sosial']);
+Route::post('/tambah_bimbingan_karir', [NoTokenApiController::class, 'tambah_bimbingan_karir']);
+
+
+Route::put('/ubah_bimbingan_pribadi/{id}', [NoTokenApiController::class, 'ubah_bimbingan_pribadi']);
+Route::put('/ubah_bimbingan_belajar/{id}', [NoTokenApiController::class, 'ubah_bimbingan_belajar']);
+Route::put('/ubah_bimbingan_sosial/{id}', [NoTokenApiController::class, 'ubah_bimbingan_sosial']);
+Route::put('/ubah_bimbingan_karir/{id}', [NoTokenApiController::class, 'ubah_bimbingan_karir']);
+
+
+Route::delete('/hapus_bimbingan_pribadi/{id}', [NoTokenApiController::class, 'hapus_bimbingan_pribadi']);
+Route::delete('/hapus_bimbingan_belajar/{id}', [NoTokenApiController::class, 'hapus_bimbingan_belajar']);
+Route::delete('/hapus_bimbingan_sosial/{id}', [NoTokenApiController::class, 'hapus_bimbingan_sosial']);
+Route::delete('/hapus_bimbingan_karir/{id}', [NoTokenApiController::class, 'hapus_bimbingan_karir']);
+
+
+Route::get('/count_bimbingan_pribadi/{id}', [NoTokenApiController::class, 'count_bimbingan_pribadi']);
+Route::get('/count_bimbingan_belajar/{id}', [NoTokenApiController::class, 'count_bimbingan_belajar']);
+Route::get('/count_bimbingan_sosial/{id}', [NoTokenApiController::class, 'count_bimbingan_sosial']);
+Route::get('/count_bimbingan_karir/{id}', [NoTokenApiController::class, 'count_bimbingan_karir']);
